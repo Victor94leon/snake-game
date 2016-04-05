a5511608f0d707dfcd6eb017c2d82cbfcfc0ee03
Víctor <victorlopez94leon@gmail.com>
2016-04-05 09:55:31 +0200

    Creada la clase Snake con atributos y método constructor

---
+++ Snake.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import java.util.ArrayList;
+public class Snake
+{
+    private int altoLienzo;
+    private int anchoLienzo;
+    private ArrayList<Segment> segmentos;
+    private int SEGMENTOS_INICIALES = 3;
+    private static final int DIFERENCIA_DE_GRADOS_ENTRE_DIRECCIONES = 90;
+    private static final int MARGEN_LIENZO = 10;
+    /**
+     * Constructor for objects of class Snake
+     */
+    public Snake(int altoLienzo,int anchoLienzo)
+    {
+        this.altoLienzo = altoLienzo;
+        this.anchoLienzo = anchoLienzo;
+    }
+}
